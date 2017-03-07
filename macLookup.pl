001ebf0bdf51dff30d549887fc59624d9e662428
marshallcooper <marshallcooper@gmail.com>
2017-03-06 23:06:35 -0800

    Added "not found" statement else clause.

--- macLookup.pl
+++ macLookup.pl
@@ -1,10 +1,16 @@
 use LWP::Simple;
 
 my $macAddress;
+my $contents;
 
 print "\nPlease enter your MAC address: \n\n";
 chomp($macAddress = <STDIN>);
 
 $contents = get("http://api.macvendors.com/$macAddress");
 
-print "\n$contents\n\n";
+if ($contents) {
+  print "\nVendor: $contents\n\n";
+}
+else {
+  print "\nNot found.\n\n";
+}
